5a7b0398079b8798d6cb464be337a1cede824fa2
Vicky <Vicky@Vicky-PC>
2014-11-26 12:49:01 -0800

    new comments page

--- GroupGoal/src/com/cs121/groupgoal/CommentActivity.java
+++ GroupGoal/src/com/cs121/groupgoal/CommentActivity.java
@@ -1,16 +1,26 @@
 package com.cs121.groupgoal;
 
-import android.annotation.SuppressLint;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.util.Log;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -19,158 +29,83 @@ import android.widget.Toast;
 
 import com.parse.ParseACL;
 import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+
 public class CommentActivity extends Activity {
 	  private EditText postEditText;
 	  private Button postButton;
-	  // Adapter for the Parse query
-	  private ParseQueryAdapter<Comment> commentsQueryAdapter;
-	 //private ArrayList<Comment> commentList;
-
-	  
+	  GoalPost currentGoal;
+	  ArrayList<String> allComments;
+	  private String commentText;
+	  String[] commentsMade;
+	  String[] commentsMadeR;
+	  private String goalID;
+	  ParseUser user= ParseUser.getCurrentUser();
+	  	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comment);
-
-	    // Set up a customized query
-	    ParseQueryAdapter.QueryFactory<Comment> factory =
-	    		
-	        new ParseQueryAdapter.QueryFactory<Comment>() {
-	    	
-	          public ParseQuery<Comment> create() {
-	           
-	            ParseQuery<Comment> query = ParseQuery.getQuery("Comments");
-	            query.include("user");
-	            System.out.println("inside factory");
-	          
-	            return query;
-	          }
-	        };
-	     // Set up the query adapter
-	        commentsQueryAdapter = new ParseQueryAdapter<Comment>(this, factory) {
-	        
-	        	@Override
-	          public View getItemView(Comment post, View view, ViewGroup parent) {
-
-	            if (view == null) {
-	              view = View.inflate(getContext(), R.layout.activity_comment_view, null);
-	            }
-	            
-	            
-	            //String[] goalOwnerFirstLast = post.getUser().toString().split("\\^");
-	            TextView contentView = (TextView) view.findViewById(R.id.content_view);
-	            TextView usernameView = (TextView) view.findViewById(R.id.username_view);
-	            contentView.setText(post.getText());
-	            String[] fullName = post.getUser().get("fullName").toString().split("\\^");
-	            usernameView.setText(fullName[0] + " " + fullName[1]);
-	            
-	            return view;
-	          }
-	        };
-
-	        // Disable automatic loading when the adapter is attached to a view.
-	      //  commentsQueryAdapter.setAutoload(false);
-
-	        // Disable pagination, we'll manage the query limit ourselves
-	       // commentsQueryAdapter.setPaginationEnabled(false);
-	        //Log.d("a", "5");
-	        
-	       
-	        // Attach the query adapter to the view
-	        ListView postsListView = (ListView) findViewById(R.id.comments_list_view);
-	        postsListView.setAdapter(commentsQueryAdapter);
-	        Log.d("a", "6");
-	    getActionBar().setDisplayHomeAsUpEnabled(true);
-	    getActionBar().setDisplayShowHomeEnabled(false);
-
-
-	    Intent intent = getIntent();
-
+		
+		
+		Intent intent = getIntent();
+		goalID = intent.getStringExtra("goalObjectId");
+		String currentGoalId = intent.getStringExtra("goalObjectId"); //gets the objectID of the goal we were viewing
+	
+		ParseQuery<GoalPost> postQuery = ParseQuery.getQuery(GoalPost.class); //fetch the GoalPost object associated with the obejctId	    
+	    try {
+	    	currentGoal = postQuery.get(currentGoalId);
+	    } catch (com.parse.ParseException e) {
+			Log.e("Goal Error", e.getMessage());
+		}
+   
+	    allComments = (ArrayList<String>) currentGoal.get("commentsList"); //this array holds the strings of comments posted
+	    
+	    commentsMade = new String[allComments.size()];
+	    commentsMade = allComments.toArray(commentsMade);
+	    Collections.reverse(Arrays.asList(commentsMade)); //want to reverse the array that stores the comments so that the 
+	    commentsMadeR = commentsMade;					  // most recent comments appear at the top of the page
+	   
+	    System.out.println("string array size "+commentsMade.length);
+	    
+	    //make listview from string array
+	    ArrayAdapter adapter = new ArrayAdapter<String>(this, 
+	    	      R.layout.comments_post_item, commentsMadeR);
+	    	      
+	    	      ListView listView = (ListView) findViewById(R.id.comments_list_view);
+	    	      listView.setAdapter(adapter);
 
 		postEditText = (EditText) findViewById(R.id.edit_comment);
-	    
 	  
 	    postButton = (Button) findViewById(R.id.submit_comment_button);
 	    postButton.setOnClickListener(new View.OnClickListener() {
 	      public void onClick(View v) {
-
-	        post();
+	    	  post();
 	      }
 	    });
-
-
 	  }
 
-	  private void post () {
-	  
+	  private void post () {	  
 	    String text = postEditText.getText().toString();
-	    // Set up a progress dialog
-	    final ProgressDialog dialog = new ProgressDialog(CommentActivity.this);
-	    dialog.setMessage(getString(R.string.progress_post));
-	    dialog.show();
+	    String userFullName = user.getString("fullName").toString();
+		String[] firstLast = userFullName.split("\\^");
+		String finalName = (firstLast[0] + " " + firstLast[1]); //we add the user's full name to the beginning of the comment so
+		String nameAndComment = (finalName + ": " + text);      // we know who made that comment 
+
+	    currentGoal.add("commentsList", nameAndComment);
+	    currentGoal.saveInBackground();
 	    
-	    commentsQueryAdapter.setTextKey("text");
-	   
-	    //Create a comment
-	    Comment c = new Comment();
-
-	    c.setText(text);
-	    //c.setGoal(g)
-	    c.setUser(ParseUser.getCurrentUser());
-
-	    ParseACL acl = new ParseACL();
-	    // Give public read access
-
-	    acl.setPublicReadAccess(true);
-
-	    c.setACL(acl);
-	   // ListView postsListView = (ListView) findViewById(R.id.comments_listview);
-	    commentsQueryAdapter.notifyDataSetChanged();
-	    // Save the comment
-	    c.saveInBackground(new SaveCallback() {
-	      @Override
-	      public void done(ParseException e) {
-	       dialog.dismiss();
-	        finish();
-	      }
-	    });
-	
-		Toast.makeText(getApplicationContext(), "Comment Successfully Created", 
+		Toast.makeText(getApplicationContext(), "Comment successfully posted", 
 				Toast.LENGTH_LONG).show();
 		
 	    Intent i = new Intent(CommentActivity.this, CommentActivity.class);
+	    i.putExtra("goalObjectId", goalID);
 		startActivity(i);
 	  }
-	  
-	@SuppressLint("NewApi")
-	@Override
-	  public boolean onOptionsItemSelected(MenuItem item) {
-		  switch (item.getItemId()) {
-		    // Respond to the action bar's Up/Home button
-		    case android.R.id.home:
-		        Intent upIntent = NavUtils.getParentActivityIntent(this);
-		        if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-		            // This activity is NOT part of this app's task, so create a new task
-		            // when navigating up, with a synthesized back stack.
-		            TaskStackBuilder.create(this)
-		                    // Add all of this activity's parents to the back stack
-		                    .addNextIntentWithParentStack(upIntent)
-		                    // Navigate up to the closest parent
-		                    .startActivities();
-		        } else {
-		            // This activity is part of this app's task, so simply
-		            // navigate up to the logical parent activity.
-		            NavUtils.navigateUpTo(this, upIntent);
-		        }
-		        return true;
-		    }
-		    return super.onOptionsItemSelected(item);
-	  }
-
 	
 }
